80d835e2f453dcb81d5e48931e04d5deaaf45655
sergey.sergeev.oneclick@gmail.com <sergey.sergeev.oneclick@gmail.com@996df6b0-2aa0-11df-917d-c9ccfb47d9d5>
2010-09-30 17:52:29 +0000

    +++

--- branches/gEngineMultiRender/sources/Device.h
+++ branches/gEngineMultiRender/sources/Device.h
@@ -1,5 +1,30 @@
 #ifndef _DEVICE_H_
 #define _DEVICE_H_
 
+#include <windows.h>		
+#include <gl\gl.h>		
+#include <gl\glu.h>
+#include <string>
+#include <d3dx9.h>
+#include "Window.h"
+
+namespace Core
+{
+	class CDevice : public CWindow
+	{
+	private :
+		void CreateD3DContext();
+		void CreateOGLContext();
+	protected :
+		static LPDIRECT3DDEVICE9 m_D3DDevice;
+		static DEVICETYPE m_DeviceType;
+		void CreateDevice(unsigned int _width, unsigned int _height, unsigned int _bpp, DEVICETYPE _device_type);
+	public :
+		__forceinline static LPDIRECT3DDEVICE9 GetDevice() { return m_D3DDevice; }
+	`	__forceinline static LPDIRECT3DDEVICE9 GetDeviceType() { return m_DeviceType; }
+
+		enum DEVICETYPE { D3D = 0, OGL };
+	};
+};
 
 #endif
