e9406059c9c65c32fc4d7df32f7f5cb75b261264
sergey.sergeev.oneclick@gmail.com <sergey.sergeev.oneclick@gmail.com@996df6b0-2aa0-11df-917d-c9ccfb47d9d5>
2011-08-19 12:28:08 +0000

    test_vk

---
+++ branches/test_vk/vkontakte_gifts_client/Connection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace vkontakte_gifts
+{
+   
+    class Connection
+    {
+        private const int k_PORT = 10000;
+        private const string k_IP = "localhost";
+
+        private string m_login = "login";
+        private string m_password = "password";
+
+        private TcpClient m_client = null;
+        private NetworkStream m_networkStream = null;
+
+        public string login
+        {
+            set
+            {
+                m_login = value;
+            }
+        }
+
+        public string password
+        {
+            set
+            {
+                m_password = value;
+            }
+        }
+
+        public Connection()
+        {
+
+        }
+
+        public void connect()
+        {
+            m_client = new TcpClient();
+
+            try
+            {
+                m_client.Connect(k_IP, k_PORT);
+                m_networkStream = m_client.GetStream();
+            }
+            catch (SocketException)
+            {
+            
+            }
+            catch (System.IO.IOException)
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            } 
+        }
+
+        public void receive()
+        {
+
+        }
+
+        public void send()
+        {
+            if (m_networkStream.CanWrite && m_networkStream.CanRead)
+            {
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(m_login);
+                m_networkStream.Write(sendBytes, 0, sendBytes.Length);
+                sendBytes = Encoding.ASCII.GetBytes(":::");
+                m_networkStream.Write(sendBytes, 0, sendBytes.Length);
+                sendBytes = Encoding.ASCII.GetBytes(m_password);
+                m_networkStream.Write(sendBytes, 0, sendBytes.Length);
+                m_networkStream.Close();
+                m_client.Close();
+            }
+            else
+            {
+                m_client.Close();
+            }
+        }
+    }
+}
---
+++ branches/test_vk/vkontakte_gifts_client/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace vkontakte_gifts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.lPassword = new System.Windows.Forms.Label();
+            this.lLogin = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.etPassword = new System.Windows.Forms.TextBox();
+            this.etLogin = new System.Windows.Forms.TextBox();
+            this.lStatus = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gbLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbLogin
+            // 
+            this.gbLogin.Controls.Add(this.lPassword);
+            this.gbLogin.Controls.Add(this.lLogin);
+            this.gbLogin.Controls.Add(this.btnLogin);
+            this.gbLogin.Controls.Add(this.etPassword);
+            this.gbLogin.Controls.Add(this.etLogin);
+            this.gbLogin.Location = new System.Drawing.Point(152, 12);
+            this.gbLogin.Name = "gbLogin";
+            this.gbLogin.Size = new System.Drawing.Size(464, 128);
+            this.gbLogin.TabIndex = 0;
+            this.gbLogin.TabStop = false;
+            // 
+            // lPassword
+            // 
+            this.lPassword.AutoSize = true;
+            this.lPassword.Location = new System.Drawing.Point(33, 68);
+            this.lPassword.Name = "lPassword";
+            this.lPassword.Size = new System.Drawing.Size(59, 13);
+            this.lPassword.TabIndex = 4;
+            this.lPassword.Text = "Password :";
+            // 
+            // lLogin
+            // 
+            this.lLogin.AutoSize = true;
+            this.lLogin.Location = new System.Drawing.Point(33, 16);
+            this.lLogin.Name = "lLogin";
+            this.lLogin.Size = new System.Drawing.Size(81, 13);
+            this.lLogin.TabIndex = 3;
+            this.lLogin.Text = "Login ( e-mail ) :";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(294, 42);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(118, 65);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // etPassword
+            // 
+            this.etPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.etPassword.Location = new System.Drawing.Point(33, 87);
+            this.etPassword.Name = "etPassword";
+            this.etPassword.PasswordChar = '*';
+            this.etPassword.Size = new System.Drawing.Size(219, 20);
+            this.etPassword.TabIndex = 1;
+            // 
+            // etLogin
+            // 
+            this.etLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.etLogin.Location = new System.Drawing.Point(33, 42);
+            this.etLogin.Name = "etLogin";
+            this.etLogin.Size = new System.Drawing.Size(219, 20);
+            this.etLogin.TabIndex = 0;
+            // 
+            // lStatus
+            // 
+            this.lStatus.AutoSize = true;
+            this.lStatus.Location = new System.Drawing.Point(118, 318);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(75, 13);
+            this.lStatus.TabIndex = 2;
+            this.lStatus.Text = "Server status :";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(12, 168);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(159, 168);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(457, 128);
+            this.panel1.TabIndex = 8;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(364, 33);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(248, 33);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(133, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 342);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.gbLogin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gift Service";
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox etPassword;
+        private System.Windows.Forms.TextBox etLogin;
+        private System.Windows.Forms.Label lStatus;
+        private System.Windows.Forms.Label lPassword;
+        private System.Windows.Forms.Label lLogin;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
