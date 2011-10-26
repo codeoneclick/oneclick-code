990fd93edf365df115497ed15b150088accd0159
sergey.sergeev.oneclick@gmail.com <sergey.sergeev.oneclick@gmail.com@996df6b0-2aa0-11df-917d-c9ccfb47d9d5>
2011-10-26 19:15:10 +0000

    +++ Box2D

--- tags/g_engine.iphone/Classes/CSceneEngine.cpp
+++ tags/g_engine.iphone/Classes/CSceneEngine.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include "CSceneEngine.h"
+#include "CCamera.h"
 
 CSceneEngine* CSceneEngine::m_engine = NULL;
 
@@ -23,7 +24,43 @@ CSceneEngine* CSceneEngine::Instance()
 
 CSceneEngine::CSceneEngine()
 {
+    float fWidth = CCamera::Instance()->Get_Width();
+    float fHeight = CCamera::Instance()->Get_Height();
+	b2Vec2 gravity;
+	gravity.Set(0.0f, -9.8f);
     
+	// Construct a world object, which will hold and simulate the rigid bodies.
+	m_pPhysicWorldController = new b2World(gravity);
+    
+	m_pPhysicWorldController->SetContinuousPhysics(true);
+    
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0, 0); // bottom-left corner
+    
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = m_pPhysicWorldController->CreateBody(&groundBodyDef);
+    
+	// Define the ground box shape.
+	b2EdgeShape groundBox;
+    
+	// bottom
+	groundBox.Set(b2Vec2(0,0), b2Vec2(fWidth,0));
+	groundBody->CreateFixture(&groundBox, 0);
+    
+	// top
+	groundBox.Set(b2Vec2(0,fHeight), b2Vec2(fWidth,fHeight));
+	groundBody->CreateFixture(&groundBox, 0);
+    
+	// left
+	groundBox.Set(b2Vec2(0,fHeight), b2Vec2(0,0));
+	groundBody->CreateFixture(&groundBox, 0);
+    
+	// right
+	groundBox.Set(b2Vec2(fWidth,fHeight), b2Vec2(fWidth,0));
+	groundBody->CreateFixture(&groundBox, 0);
 }
 
 CSceneEngine::~CSceneEngine()
