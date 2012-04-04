d7fe217916bc87376773cefb4e4356e41206770b
sergey.sergeev.oneclick@gmail.com <sergey.sergeev.oneclick@gmail.com@996df6b0-2aa0-11df-917d-c9ccfb47d9d5>
2012-04-04 18:24:21 +0000

    remove

--- trunk/iGaia/main.m
+++
@@ -1,8 +0,0 @@
-#define GLM_MESSAGES
-int main(int argc, char *argv[])
-{
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, @"MainAppDelegate");
-    [pool release];
-    return retVal;
-}
